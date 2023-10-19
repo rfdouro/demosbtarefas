b04ebd785cb0158c8d3602c69d232ad37b14fa7f
Rômulo F. Douro <rfdouro@gmail.com>
2023-10-19 14:57:25 -0300

    deploy

--- src/main/java/br/org/rfdouro/demosbtarefas/control/TarefaController.java
+++ src/main/java/br/org/rfdouro/demosbtarefas/control/TarefaController.java
@@ -5,6 +5,7 @@ import br.org.rfdouro.demosbtarefas.model.TarefaRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author romulo
  */
 @RestController
+@CrossOrigin
 @RequestMapping("/tarefa")
 public class TarefaController {
 
